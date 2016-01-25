fa8b698b3fb28970a6bc5b7c12d47abc4c48f07c
Charles Troupin <ctroupin@socib.es>
2016-01-25 14:52:30 +0100

    exercise

--- exercises/solutions/read_textfile.py
+++ exercises/solutions/read_textfile.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-
+"""The program reads a two-column text file
+and print each column separately.
+Note: don't use numpy or other module.
+"""
 
 def read_profile(datafile):
   depth = []
@@ -14,9 +17,11 @@ def read_profile(datafile):
   return depth, temperature
 
 def main():
-  datafile = "../notebooks/data/depth_temperature1.dat"
+  datafile = '../../notebooks/data/depth_temperature1.dat'
   depth, temperature = read_profile(datafile)
+  print 'depth:'
   print depth
+  print 'temperature:'
   print temperature
 
 if __name__ == '__main__':
